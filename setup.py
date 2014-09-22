223ed6fe038f4e37c95da2287b53d475c02a6a40
unknown <passmoj@DUN7Q325Q1.sealedair.corp>
2014-09-21 21:22:47 -0400

    Update NumPy, SciPy, remove hgignore.  Edit setup.py to include maintainer.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_data_files(dirname):
             flist.append(osp.join(dirpath, fname))
     return flist
 
-setup(name='Scidoc', version='1.8.0',
+setup(name='Scidoc', version='1.8.1',
       description='Scidoc installs scientific libraries documentation',
       long_description="""Scidoc installs scientific libraries documentation 
 (NumPy, SciPy, Matplotlib and others) in sys.prefix\Doc directory on Windows. 
@@ -21,5 +21,7 @@ Scidoc version is indexed to NumPy version.""",
       data_files=[(r'Doc', get_data_files('doc'))],
       author = "Pierre Raybaut",
       author_email = 'pierre.raybaut@gmail.com',
+      maintainer = 'Jim Passmore',
+      maintaner_email = 'tkd4jim@gmail.com'
       url = 'http://code.google.com/p/winpython/',
       classifiers=['Operating System :: Microsoft :: Windows'])
